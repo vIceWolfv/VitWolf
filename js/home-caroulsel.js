2a574b5fb4fd005f82afe5e0d7baa8d270a3f12a
Pablo Vitale <pablovitale89@gmail.com>
2019-09-17 23:14:03 -0300

    repo1

---
+++ js/home-caroulsel.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    pagination:false,
+    autoplay:500,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2
+        }
+    }
+})
